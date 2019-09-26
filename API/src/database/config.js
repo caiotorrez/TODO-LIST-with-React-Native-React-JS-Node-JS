c92c8a75f9d4f9476ee5bcab8889904a485620d2
Caio <caiotorrez@live.com>
2019-09-25 22:12:53 -0300

    base aplication configured

---
+++ API/src/database/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  host: '127.0.0.1',
+  username: 'docker',
+  password: 'docker',
+  database: 'nodeauth',
+  dialect: 'mysql',
+  operatorsAliases: false,
+  logging: true,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  }
+}
