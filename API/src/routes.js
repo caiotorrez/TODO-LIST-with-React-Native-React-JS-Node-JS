150802067e91eab5a0389322b35a9cfb1eff3ffd
Caio <caiotorrez@live.com>
2019-09-29 22:11:46 -0300

    setup db reconfigured

--- API/src/routes.js
+++ API/src/routes.js
@@ -1,3 +1,6 @@
 const routes = require('express').Router();
+const { User } = require('./models');
+
+User.create({name: 'Caio', email: 'caiotorrez@live.com', password_hash: '131232131'});
 
 module.exports = routes;
