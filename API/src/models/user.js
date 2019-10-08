97bc3ca670f759603eacdf1e10dda3b65f4e4827
Caio <caiotorrez@live.com>
2019-10-08 09:16:34 -0300

    added prototypes -> checkpassword, generateToken, and replace toJSON not return password

--- API/src/models/user.js
+++ API/src/models/user.js
@@ -1,9 +1,28 @@
+
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
 module.exports = (sequelize, DataTypes) => {
     const User = sequelize.define("User", {
         name: DataTypes.STRING,
         email: DataTypes.STRING,
         password_hash: DataTypes.STRING
     });
+
+    User.prototype.checkPassword = function (password) {
+        return bcrypt.compare(password, this.password_hash);
+    }
+
+    User.prototype.generateToken = function () {
+        return jwt.sign({ id: this.id }, 'secret_key')
+    }
+
+    User.prototype.toJSON = function () {
+        var values = Object.assign({}, this.get());
+
+        delete values.password_hash;
+        return values;
+    }
 
     return User;
 }
