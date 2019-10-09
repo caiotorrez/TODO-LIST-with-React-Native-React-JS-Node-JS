ae54143705f3645955e318ef502eb86ff562f9ab
Caio <caiotorrez@live.com>
2019-10-08 22:24:07 -0300

    added auth middleware,  findOneUser by Email and FindAllUsers

--- API/src/routes.js
+++ API/src/routes.js
@@ -1,10 +1,16 @@
 const routes = require('express').Router();
 const SessionController = require('./controllers/session.controller');
 const UserController = require('./controllers/user.controller');
+const authMiddleware = require('./middleware/auth.middleware');
 
+// public routes
 routes.post('/session', SessionController.authenticate);
 routes.post('/users', UserController.create);
 
+routes.use(authMiddleware); // check authentication
 
+// private routes 
+routes.get('/users/:email', UserController.findOneByEmail);
+routes.get('/users/', UserController.findAll);
 
 module.exports = routes;
