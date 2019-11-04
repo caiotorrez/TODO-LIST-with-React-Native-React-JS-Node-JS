8944e20145e33886a045d2c36e4b4a26d489ee2e
Caio <caiotorrez@live.com>
2019-11-04 14:21:42 -0300

    base task migrate

---
+++ API/src/database/migrations/20191103144021-task.js
@@ -0,0 +1,57 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('tasks', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      start: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      end: {
+        type: Sequelize.DATE,
+      },
+      description: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      completed: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false
+      },
+      active: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: true
+      },
+      release: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: true
+      },
+      user_id: {
+        type: Sequelize.INTEGER
+      },
+      // Timestamps
+      created_at: {
+        type: Sequelize.DATE(3),
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP(3)'),
+      },
+      updated_at: {
+        type: Sequelize.DATE(3),
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3)'),
+      },
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('tasks');
+  }
+};
